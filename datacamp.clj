27a63dc8da301c3120d4b17919f879863c18d642
Ruslan Khafizov <ruslan.khafizov@gmail.com>
2024-10-13 16:04:19 +0800

    Script for datacamp

---
+++ datacamp.clj
@@ -0,0 +1,66 @@
+(require '[babashka.http-client :as http]
+         '[babashka.json :as json]
+         '[lambdaisland.uri :refer [uri]]
+         '[babashka.fs :as fs]
+         '[selmer.parser :as selmer]
+         '[babashka.pods :as pods]
+         '[meander.epsilon :as m])
+(pods/load-pod 'retrogradeorbit/bootleg "0.1.9")
+
+(require
+ '[pod.retrogradeorbit.bootleg.utils :as bootleg]
+ '[pod.retrogradeorbit.hickory.select :as s])
+
+(def page-type-map
+  {"HEADER" :header
+   "TEXT" :text
+   "TEST" :test})
+
+(def headers-map-static
+  {:headers
+   {"accept" "application/json"
+    "authority" "aisystant.system-school.ru"
+    "referer" "https://aisystant.system-school.ru/lk/"
+    "user-agent" "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"}})
+
+(def headers-map
+  (conj (:headers headers-map-static)
+        ["dct" (slurp "dct.txt")]))
+
+(def importing-data
+  (json/read-str (slurp "course.json")))
+
+(:course importing-data)
+
+(def chapters
+  (get-in importing-data [:course :chapters]))
+
+(map #(select-keys % [:title :number]) chapters)
+
+(def chapter-1
+  (first chapters))
+
+(first (:exercises chapter-1))
+
+(def exercise-1 (first (:exercises chapter-1)))
+
+(defn is-video? [exercise]
+  (= "VideoExercise" (:type exercise)))
+
+(def exercise-keys [:title :url])
+
+(->> (:exercises chapter-1)
+     (filter is-video?)
+     (map #(select-keys % exercise-keys)))
+
+(def video-page-raw
+  (slurp "page.html"))
+
+(second (re-find #"embedUrl\":\s+\"(.*?)\"" video-page-raw))
+
+(def projector-page
+  (bootleg/convert-to (slurp "video-page.html") :hickory-seq))
+
+(s/select (s/id "videoData")
+          (second projector-page))
+(s/select (s/tag :table) projector-page)
